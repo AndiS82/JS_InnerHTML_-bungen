f4f53df30d037bfccd17ab16a08a15ec4475ac39
AndiS82 <19schneiderandreas82@gmail.com>
2022-10-06 15:06:32 +0200

    fertig

---
+++ Lev1_3/assets/js/main.js
@@ -0,0 +1,3 @@
+document.getElementById("gallery").innerHTML = '<figure><img src="https://unsplash.it/200/200"><figcaption>Fig1</figcaption></figure><figure><img src="https://unsplash.it/200/200"><figcaption>Fig2</figcaption></figure><figure><img src="https://unsplash.it/200/200"><figcaption>Fig3</figcaption></figure>'
+
+document.getElementById("gallery").style.display = "flex";
